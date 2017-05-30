7178423d0e7de5e6cd4e9dcb920d4856f6ceafcb
Tohru Ochiai <kyukoh@gmail.com>
2017-05-30 13:01:58 +0900

    dry-runモード追加

--- Framework/tools/rsync_sample_app.sh
+++ Framework/tools/rsync_sample_app.sh
@@ -6,6 +6,11 @@ export SRC_PORT=2222
 export SRC_HOST=main.jp-yushun@ssh461.lolipop.jp
 export SRC_ROOT=/home/users/1/main.jp-yushun/web/app00.kyukoh.net/club.kyukoh.net/
 
-comm="sshpass -p $SRC_PASSWD rsync -rlcgzopDv --files-from=sync_files.txt --exclude='._*' -e 'ssh -p $SRC_PORT' $SRC_HOST:$SRC_ROOT $APP_ROOT"
-#comm="sshpass -p $SRC_PASSWD rsync -rlcgzopDv --files-from=sync_files.txt --exclude='._*' -e $SRC_HOST:$SRC_PORT:$SRC_ROOT $APP_ROOT"
+export dry_run="--dry-run"
+if [ "$2" = "run" ]; then
+    export dry_run=""
+fi
+
+comm="sshpass -p $SRC_PASSWD rsync $dry_run -rlcgzopDv --files-from=sync_files.txt --exclude='._*' -e 'ssh -p $SRC_PORT' $SRC_HOST:$SRC_ROOT $APP_ROOT"
 echo $comm
+$comm
